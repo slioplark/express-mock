2a3008888fd88e3378024c44fcd79da1e0a20fc1
Mason <mason.chang@cimforce.com>
2020-06-20 20:06:08 +0800

    feat: npm install mock.js

--- routes/api/test.js
+++ routes/api/test.js
@@ -1,10 +1,19 @@
 var router = require('express').Router();
+var Mock = require('mockjs')
 
 router.get('/', (req, res, next) => {
   return res.json({
     api: '/api/test',
     method: 'get',
   });
+});
+
+router.get('/mock', (req, res, next) => {
+  return res.json(Mock.mock({
+    'list|1-10': [{
+      'id|+1': 1
+    }]
+  }));
 });
 
 module.exports = router;
