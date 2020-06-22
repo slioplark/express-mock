f9313fa24cae35e991b10366c2252ef0c38b5eff
Mason <jeailfy9520@outlook.com>
2020-06-23 01:30:16 +0800

    feat: error handler

--- app.js
+++ app.js
@@ -37,7 +37,10 @@ app.use(function (err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.json({
+    status: err.status,
+    message: err.message,
+  });
 });
 
 module.exports = app;
